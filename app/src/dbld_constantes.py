19488189d1a7171bf4ca6c76427462699127108f
jrangelf <jrangelf@gmail.com>
2023-05-26 14:49:48 -0300

    Update dbld_constantes.py

--- app/src/dbld_constantes.py
+++ app/src/dbld_constantes.py
@@ -6,21 +6,12 @@ def processString(txt):
     txt = txt.replace(specialChar, '')
   return txt
 
-
-# Dados conexão sql server
-#SQL_USER = 'SVC_PGU_ATUACAO_WEB'
-#SQL_PASSWORD = 'QS07O$nCFFD5*uJegaZ'
-#SQL_SERVER = '10.207.112.5'
-#SQL_DB = 'PGU_ESTATISTICA'
-#SQL_PORT = 1433
-
 # Dados conexão sql server
 SQL_USER = config('SQL_USER')
 SQL_PASSWORD = config('SQL_PASSWORD')
 SQL_SERVER = config('SQL_SERVER')
 SQL_DB = config('SQL_DB')
 SQL_PORT = config('SQL_PORT')
-
 
 retira = lambda a: a.split(';')
 
